56afea1628ff8a959b02ef9fe663adf3d5f5ec71
Patricio <pcifrey.lab@gmail.com>
2024-11-17 02:10:16 +0100

    wip: equis scrappin (close to finish it)

--- scrappin/salaequis.mjs
+++ scrappin/salaequis.mjs
@@ -1,32 +1,40 @@
 import {chromium} from "playwright"
 import { writeFile } from 'fs'
 
-console.log('start')
 const browser = await chromium.launch({headless: false})
 const page = await browser.newPage()
 await page.goto('https://www.filmaffinity.com/es/theater-showtimes.php?id=1261')
 await page.locator('button.css-113jtbf').nth(1).click()
-const selectors = page.locator('.btn.btn-sm.btn-outline-secondary.d-flex.justify-content-between.see-more')
-const count = await selectors.count()
-console.log(count)
+
+let selectors = page.locator('.btn.see-more')
+let count = await selectors.count()
+
 for (let i = 0; i<count; i++){
+    console.log('count inicio bucle: ', count)
     await selectors.nth(i).scrollIntoViewIfNeeded()
-    await selectors.nth(i).click()
-    await page.waitForTimeout(100)
-    console.log('click')
+    await selectors.nth(i).waitFor({ state: 'visible', timeout: 15000 })
+    await selectors.nth(i).click({timeout: 5000})
+    await page.waitForTimeout(5000)
+
+    selectors = page.locator('.btn.see-more')
+    count =  await selectors.count()
+
 }
 
 const films = await page.$$eval('.movie', results =>{
-        return results.map(el =>{
+    return results.map(el =>{
             const poster = el.querySelector('img')?.src || ''
-            const dateInfo = Array.from(el.querySelectorAll('.row.g-0.mb-2')).map(day => day.getAttribute('data-sess-date') || '')
-            const filmTitle = el.querySelector('.fs-5')?.textContent || ''
+            const dateInfo = Array.from(
+                el.querySelectorAll('.row.g-0.mb-2')).map(day =>
+                day.getAttribute('data-sess-date') || null
+            )
+            const filmTitle = el.querySelector('.fs-5')?.textContent || null
             const director = Array.from(el.querySelectorAll('.mc-flex .mc-info-container .mc-director a')).map(dir => dir.textContent.trim() || '')
             const linkToPurchase = el.querySelector('.btn.btn-sm.btn-outline-secondary')?.href || ''
-            return {poster, dateInfo, filmTitle, director, linkToPurchase}
+            return { filmTitle, director, dateInfo, poster, linkToPurchase}
         })
 })
-console.log('cerramos')
+
 await browser.close()
 writeFile('films.json',
     JSON.stringify(films, null, 2), 'utf8', (error) =>{})
