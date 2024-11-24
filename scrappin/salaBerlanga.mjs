fd5a15b49295300f2e32577bba9fc437d055b25d
Patricio <pcifrey.lab@gmail.com>
2024-11-24 18:43:25 +0100

    wip: sala berlanga

--- scrappin/salaBerlanga.mjs
+++ scrappin/salaBerlanga.mjs
@@ -6,21 +6,17 @@ const page = await browser.newPage()
 await page.goto('https://cine.entradas.com/cine/madrid/sala-berlanga/sesiones')
 await page.locator('button.button-primary.button-sm').nth(2).click()
 
-let initialPosition = 0
-let finalPosition = 999999999
+const scrollToEnd = async () => {
 
-const scrollTo = (startPosition, EndPosition) =>{
-    window.scrollTo(startPosition, EndPosition)
+    let initialPosition = 0
+    let finalPosition = 1
+    while (initialPosition !== finalPosition) {
+        finalPosition = await page.evaluate(() => {
+            initialPosition = window.scrollY
+            window.scrollBy(0, window.innerHeight)
+            return document.body.innerHeight
+        })
+    }
 }
 
-const scrollPositions = async () => {
-    await page.evaluate(() => {
-        initialPosition = window.scrollY
-        scrollTo(initialPosition, document.body.scrollHeight)
-        finalPosition = window.scrollY
-    })
-}
-
-while (initialPosition !== finalPosition) {
-    scrollPositions()
-}
+scrollToEnd()
