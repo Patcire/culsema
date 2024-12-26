bb918331b8e3ec465f1501b37aab3cd57fe18f31
Patricio <pcifrey.lab@gmail.com>
2024-12-26 12:59:35 +0100

    berlanga finished

--- scrappin/salaBerlanga.mjs
+++ scrappin/salaBerlanga.mjs
@@ -1,40 +1,42 @@
-import {chromium} from "playwright"
+import { chromium } from "playwright"
 import { writeFile } from 'fs'
 
-const browser = await chromium.launch({headless: false})
+const browser = await chromium.launch({ headless: false })
 const page = await browser.newPage()
 await page.goto('https://cine.entradas.com/cine/madrid/sala-berlanga/sesiones')
 await page.locator('button.button-primary.button-sm').nth(2).click()
-const footer = await page.locator('footer')
-
 
 const scrollToEnd = async () => {
     await page.evaluate(async () => {
-        const isFooterVisible = async () => {
-            return footer.evaluate((el) => {
-                return el.getBoundingClientRect().bottom <= document.body.scrollHeight
-            })
-        }
-
-        while ( window.innerHeight + window.scrollY < document.body.scrollHeight
-                &&
-                !await isFooterVisible() ) {
-            await window.scrollBy(0, window.innerHeight)
+        while (window.innerHeight + window.scrollY < document.body.scrollHeight) {
+            window.scrollBy(0, window.innerHeight)
+            await new Promise(resolve => setTimeout(resolve, 500))
         }
     })
-
 }
 
 await scrollToEnd()
-await page.waitForTimeout(10000)
-const films = await page.$$eval('div.py-6', results =>{
-    return results.map(movie =>{
-        const poster = movie.querySelector('img.transition-opacity')?.src || ''
+await page.waitForTimeout(1000)
 
-        return { /*filmTitle, director, dateInfo, */poster, /*linkToPurchase*/ }
+const films = await page.$$eval('div.py-6', results => {
+    return results.map(movie => {
+        const poster = movie.querySelector('img.transition-opacity')?.src || null
+        const dateInfo = Array.from(
+            movie.querySelectorAll('.w-full.items-center.space-y-3.text-center')).map(day =>
+            day.querySelector('.w-full.items-center.space-y-3.text-center > div').textContent || null
+        )
+        const filmTitle = movie.querySelector('.text-dark.space-x-2.text-xl.font-medium.leading-tight')?.textContent || ''
+        const director = null // the web don't show it
+        const linkToPurchase = Array.from(
+            movie.querySelectorAll('.space-y-2 > a'))?.map(a =>
+            'https://cine.entradas.com'+a.getAttribute('href') || null
+        )
+        return { poster, filmTitle, director, dateInfo, linkToPurchase }
     })
 })
+
 console.log(films)
+
+writeFile('films.json', JSON.stringify(films, null, 2), 'utf8', (error) => {})
+
 await browser.close()
-writeFile('films.json',
-    JSON.stringify(films, null, 2), 'utf8', (error) =>{})
