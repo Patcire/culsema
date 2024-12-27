de36ef9dadf88aa8f45f6a71b7f8ccb96fb32981
Patricio <pcifrey.lab@gmail.com>
2024-12-27 13:31:17 +0100

    wip: pequeño cine studio scrappin continuation

--- scrappin/pequeñoCineEstudio.mjs
+++ scrappin/pequeñoCineEstudio.mjs
@@ -4,19 +4,26 @@ import { writeFile } from 'fs'
 const browser = await chromium.launch({headless: false})
 const page = await browser.newPage()
 await page.goto('https://www.pcineestudio.es/en-cartel')
+await page.click('.N24JKK.bqbmwD.Pg7AcP.KiIMJv.Mm8Lm6')
 
-// wixui-rich-text__text
+await page.waitForSelector('div.Zc7IjY', { timeout: 5000 })
 
-const films = await page.$$eval('span.wixui-rich-text__text', results =>{
+const films = await page.$$eval('div.Zc7IjY', results =>{
     return results.map((movie) =>{
-        const poster = movie.querySelector('span.wixui-rich-text__text')?.split('<br>')[] || null
-        const dateInfo = movie.querySelector('span.wixui-rich-text__text')?.split('<br>')[] || null
+        const poster = movie.querySelector('.MazNVa.comp-lrtqkqs4.wixui-image img')?.src || null
+        //const dateInfo = Array.from(movie.querySelector('.HcOXKn.c9GqVL.QxJLC3.lq2cno.YQcXTT.comp-lsoxamr8.wixui-rich-text > span')?.
+        //textContent.split('<br>').forEach() || null)
         // substring start at 15 because web put before real title next text: "Título Original"
-        const filmTitle = movie.querySelector('span.wixui-rich-text__text')?.split('<br>')[0].substring(15) || null
+        // font_8 wixui-rich-text__text --> selector of all movie info
+        const filmTitle = movie.innerText.split('\n')[0]
         // substring start at 9 because web put before real title next text: "Dirección"
-        const director = movie.querySelector('span.wixui-rich-text__text')?.split('<br>')[4].substring(9) || null
-        const linkToPurchase = movie.querySelector('.wixui-button.StylableButton2545352419__link')?.getAttribute('href') || null
+        const director = movie.textContent.split('\n')[4].substring(9) || null
+        const linkToPurchase = movie.querySelector('.wixui-button.StylableButton2545352419__link')?.
+            getAttribute('href') || null
 
-        return { filmTitle, director, dateInfo, poster, linkToPurchase}
+        return { filmTitle, director, /*dateInfo*/ poster, linkToPurchase}
     })
 })
+
+await browser.close()
+writeFile('films.json', JSON.stringify(films, null, 2), 'utf8', (error)=>{})
